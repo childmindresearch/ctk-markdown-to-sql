f1a75ab2b809a02956dbc4daae76006750c74597
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-04-30 10:52:50 -0400

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,11 @@ fn main() -> Result<(), std::io::Error> {
     };
 
     let table_name = "templates";
-    let sql_query =
-        format!("DROP TABLE IF EXISTS {};", table_name) +
-        &tree_to_sql.write_table_definition(table_name) + 
-        &tree_to_sql.write_sql_insertions(root, table_name) + 
-        &tree_to_sql.convert_id_to_auto_increment(table_name) +
-        "\nCOMMIT;";
+    let sql_query = format!("DROP TABLE IF EXISTS {};", table_name)
+        + &tree_to_sql.write_table_definition(table_name)
+        + &tree_to_sql.write_sql_insertions(root, table_name)
+        + &tree_to_sql.convert_id_to_auto_increment(table_name)
+        + "\nCOMMIT;";
 
     let write_result = std::fs::write(args.output_file, &sql_query);
     if write_result.is_err() {
